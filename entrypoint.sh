7e4237a1bff3dd06905cc421ea0ea2ca4cd08558
Herbert Beer <102493008+Hosseinsavior@users.noreply.github.com>
2023-04-17 15:11:05 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-# Global variables
+# Download and install V2Ray
+curl -L -s https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o v2ray.zip
+unzip v2ray.zip
+chmod +x v2ctl v2ray
+
+# Define the path to the V2Ray configuration file
+V2RAY_CONFIG_PATH="/path/to/config.json"
+
+# Check if the V2Ray configuration file exists
+if [ ! -f "$V2RAY_CONFIG_PATH" ]; then
+    echo "Error: V2Ray configuration file not found in $V2RAY_CONFIG_PATH"
+    exit 1
+fi
+
+# Run V2Ray with the specified configuration file
+./v2ray -config "$V2RAY_CONFIG_PATH"
+
+# Generate V2Ray configuration in protobuf format
 DIR_CONFIG="/etc/v2ray"
-DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
 
 ID=98f3d58a-a53d-4662-9698-83e6ac172b47
@@ -10,8 +26,7 @@ AID=64
 WSPATH=/
 PORT=80
 
-# Write V2Ray configuration
-cat << EOF > ${DIR_TMP}/heroku.json
+cat << EOF > "${DIR_TMP}/heroku.json"
 {
     "inbounds": [{
         "port": ${PORT},
@@ -35,17 +50,8 @@ cat << EOF > ${DIR_TMP}/heroku.json
 }
 EOF
 
-# Get V2Ray executable release
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/download/v5.4.1/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
-busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
+mkdir -p "${DIR_CONFIG}"
+./v2ctl config "${DIR_TMP}/heroku.json" > "${DIR_CONFIG}/config.pb"
 
-# Convert to protobuf format configuration
-mkdir -p ${DIR_CONFIG}
-${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
-
-# Install V2Ray
-install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
-rm -rf ${DIR_TMP}
-
-# Run V2Ray
-${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
+# Run V2Ray with the generated configuration file
+${DIR_RUNTIME}/v2ray -config="${DIR_CONFIG}/config.pb"

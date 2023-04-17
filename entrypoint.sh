af26c7220512c4cb29e75fb66cdff5bda3e7c255
Herbert Beer <102493008+Hosseinsavior@users.noreply.github.com>
2023-04-17 17:27:27 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,7 +51,7 @@ cat << EOF > "${DIR_TMP}/heroku.json"
 EOF
 
 mkdir -p "${DIR_CONFIG}"
-./v2ctl config "${DIR_TMP}/heroku.json" > "${DIR_CONFIG}/config.pb"
+./v2ray config "${DIR_TMP}/heroku.json" > "${DIR_CONFIG}/config.pb"
 
 # Run V2Ray with the generated configuration file
 ${DIR_RUNTIME}/v2ray -config="${DIR_CONFIG}/config.pb"
